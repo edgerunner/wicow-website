d599300b7c6f71583a321363ddd72b2d217926c3
Mert Torun <mert@merttorun.com>
2020-07-28 11:56:39 +0300

    Write global CSS basics for the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,117 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Wicow</title>
       </Head>
 
+      <header>
+        <img src="/wicow.svg" alt="Wicow" className="logo" />
+      </header>
+
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+        <section>
+          <h1>Know your cows</h1>
+          <h2>Wicow lets you do that right here</h2>
+          
+          <p>
+            Wicow is a system for making sure that you
+            can always keep tabs on your cows, even while
+            you are away from the farm.
+          </p>
+          <p>
+            You do not even have to check anything. Wicow
+            will let you know if Bessie will be <em>calving </em> 
+            today, or if Agnes has a slight <em>fever</em>.
+          </p>
+          <p>
+            All you have to do is take five minutes to
+            install the TSens™ sensor when the time is
+            right (which Wicow will also tell you), and
+            then you can rest assured that you will know
+            when something happens.
+          </p>
+        </section>
+        <section>
+          <h1>Sit back and relax</h1>
+          <h2>Take time for your farm and yourself</h2>
+          <p>
+            There is a key feature in Wicow that sets it apart from anything else: We do not expect you to keep checking our app. We will tell you when you need to act on something.
+          </p>
+          <p>
+            More than that, whenever we can, we train and use our artificial intelligence to predict the future, and tell you what will happen in advance, before it happens.
+          </p>
+          <p>
+            The benefit for you is that you can just stop thinking about things like calving, insemination and fever. The time you spend on just checking on those could be yours to work, play or rest as you see fit.
+          </p>
           </a>
         </div>
+        </section>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        
       </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+      <style global jsx>{`
+        :root {
+          --dark: #333;
+          --darkish: #666;
+          --light: #fefefe;
+          --wicow-blue: #3FD1FF;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: "Lato", sans-serif;
+          background-color: var(--dark);
+          color: var(--light);
+        }
+
+        body { padding: 1rem; }
+
+        a {
+          color: var(--wicow-blue);
         }
 
         * {
           box-sizing: border-box;
         }
+
+        h1 { 
+          font-weight: 500;
+          font-size: 5rem;
+          line-height: 4rem;
+          margin: 2rem 0;
+          letter-spacing: -0.1ch; 
+        }
+
+        h2 {
+          font-weight: 300;
+          line-height: 2rem;
+          font-size: 2rem;
+          margin: 2rem 0;
+        }
+
+        p {
+          font-weight: 500;
+          line-height: 2rem;
+          font-size: 1.25rem;
+          margin: 2rem 0;
+        }
+
+        em { 
+          all: unset;
+          color: var(--wicow-blue);
+        }
+
+        section + section {
+            margin-top: 8rem;
+        }
       `}</style>
-    </div>
+    </>
   )
 }
