4a91a7348805c90578c266d4e35b04b64f1b1f11
Mert Torun <mert@merttorun.com>
2020-09-17 01:36:43 +0300

    Build mock API for <AlertsSummary>

--- components/AlertsSummary.js
+++ components/AlertsSummary.js
@@ -1,30 +1,51 @@
+import { useEffect, useReducer } from "react";
 import { useLocale } from "../hooks";
 
-const dummyData = {
-    perfect: 1812,
-    eventual: 23,
-    false_: 4,
-    missed: 2,
-    latest: { name: "Sarıkız", country: "TR", time: Date.now() - 86400000 }
-}
 
 export default function AlertsSummary() {
-    return <aside className="AlertsSummary">
-        <dl>
+    const [state, dispatch] = useReducer(reducer, { initial: true });
+
+    useEffect(() => {
+        if (state.initial) {
+            dispatch({type: "loading"});
+            async function fetchAlerts() {
+                const res = await fetch("/api/alerts");
+                res
+                    .json()
+                    .then(data => dispatch({type: "success", data}))
+                    .catch(error => dispatch({type: "error", error}));
+            };
+            fetchAlerts();
+        }
+    }, []);
+
+
+
+    return <aside className="AlertsSummary">{
+        state?.data 
+        ? <dl>
             <dt>Calvings accurately detected on first attempt</dt>
-            <dd><em><Number value={dummyData.perfect}/></em></dd>
+            <dd><em><Number value={state.data.perfect}/></em></dd>
 
             <dt>Calvings eventually detected on later attempts</dt>
-            <dd><Number value={dummyData.eventual}/></dd>
+            <dd><Number value={state.data.eventual}/></dd>
 
             <dt>Undetected calvings with previous false alerts</dt>
-            <dd><Number value={dummyData.false_}/></dd>
+            <dd><Number value={state.data.false_}/></dd>
 
             <dt>Undetected calvings without any alerts</dt>
-            <dd><Number value={dummyData.missed}/></dd>
+            <dd><Number value={state.data.missed}/></dd>
         </dl>
+        : <>…</>
+    }</aside>;
+}
 
-    </aside>;
+function reducer(state, action) {
+    switch (action.type) {
+        case "success": return { data: action.data };
+        case "error": return { error: action.error };
+        case "loading": return { loading: true };
+    }
 }
 
 function Number({value}) {
