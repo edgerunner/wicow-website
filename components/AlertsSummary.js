8d8154243c067e396e17d3a78d35f4126583f59b
Mert Torun <mert@merttorun.com>
2020-09-17 00:17:28 +0300

    Implement locale-based number formatting in <AlertsSummary>

--- components/AlertsSummary.js
+++ components/AlertsSummary.js
@@ -1,7 +1,9 @@
+import { useLocale } from "../hooks";
+
 const dummyData = {
     perfect: 1812,
     eventual: 23,
-    false: 4,
+    false_: 4,
     missed: 2,
     latest: { name: "Sarıkız", country: "TR", time: Date.now() - 86400000 }
 }
@@ -10,17 +12,22 @@ export default function AlertsSummary() {
     return <aside className="AlertsSummary">
         <dl>
             <dt>Calvings accurately detected on first attempt</dt>
-            <dd><em>{dummyData.perfect}</em></dd>
+            <dd><em><Number value={dummyData.perfect}/></em></dd>
 
             <dt>Calvings eventually detected on later attempts</dt>
-            <dd>{dummyData.eventual}</dd>
+            <dd><Number value={dummyData.eventual}/></dd>
 
             <dt>Undetected calvings with previous false alerts</dt>
-            <dd>{dummyData.false}</dd>
+            <dd><Number value={dummyData.false_}/></dd>
 
             <dt>Undetected calvings without any alerts</dt>
-            <dd>{dummyData.missed}</dd>
+            <dd><Number value={dummyData.missed}/></dd>
         </dl>
+
     </aside>;
 }
 
+function Number({value}) {
+    const locale = useLocale();
+    return <data value={value}>{value.toLocaleString(locale)}</data>;
+}
