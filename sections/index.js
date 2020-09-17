d3577a81d324d1865ffb247bbd886de1196d1c00
Mert Torun <mert@merttorun.com>
2020-09-17 18:58:25 +0300

    Wire up <Horsemouth> to the home page

--- sections/index.js
+++ sections/index.js
@@ -2,5 +2,6 @@ import * as KnowYourCows from './KnowYourCows';
 import * as SitBackAndRelax from './SitBackAndRelax';
 import * as Simple from './Simple';
 import * as NoBullshit from './NoBullshit';
+import * as Horsemouth from './Horsemouth';
 
-export { KnowYourCows, SitBackAndRelax, Simple, NoBullshit };
+export { KnowYourCows, SitBackAndRelax, Simple, NoBullshit, Horsemouth };
