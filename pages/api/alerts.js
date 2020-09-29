ebb99a87227f991368c32f829ae7d7f24d9e57a5
Mert Torun <mert@merttorun.com>
2020-09-29 16:31:21 +0300

    Generate time-based fake calving counts
    Development mockup. Simple percentage distribution of a time-based total

--- pages/api/alerts.js
+++ pages/api/alerts.js
@@ -1,15 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const dummyData = {
-    perfect: 1812,
-    eventual: 23,
-    false_: 5,
-    missed: 2,
-    latest: { name: "Sarıkız", country: "TR", time: Date.now() - 86400000 }
+const baseDate = Date.parse("2019-06-01");
+const calvingRate = 2.25 * 60 * 60 * 1000; //seconds in 2:15 hours
+
+function calvings(now) {
+  return Math.floor((now - baseDate) / calvingRate)
+}
+
+function dummyData(calvings) {
+    return {
+      perfect: Math.floor(calvings * 0.85),
+      eventual: Math.floor(calvings * 0.12),
+      false_: Math.floor(calvings * 0.02),
+      missed: Math.floor(calvings * 0.01)
+    }
 }
 
 export default (req, res) => {
   res
     .status(200)
-    .json(dummyData);
+    .json(dummyData(calvings(Date.now() + 604800000)));
 }
