08540f14312caeb1b6168de434ad39468ab2bf5f
Mert Torun <mert@merttorun.com>
2020-10-01 16:08:14 +0300

    Add <SmartInvestment> base text

---
+++ sections/SmartInvestment/index.js
@@ -0,0 +1,7 @@
+import { default as en } from "./en.mdx";
+import { default as de } from "./de.mdx";
+import { default as tr } from "./tr.mdx";
+
+const id = "smart-investment";
+
+export { en, de, tr, id };
--- sections/index.js
+++ sections/index.js
@@ -2,6 +2,10 @@ import * as KnowYourCows from './KnowYourCows';
 import * as SitBackAndRelax from './SitBackAndRelax';
 import * as Simple from './Simple';
 import * as NoNonsense from './NoNonsense';
+import * as SmartInvestment from './SmartInvestment';
 import * as Horsemouth from './Horsemouth';
 
-export { KnowYourCows, SitBackAndRelax, Simple, NoNonsense, Horsemouth };
+export { 
+    KnowYourCows, SitBackAndRelax, Simple, 
+    NoNonsense, SmartInvestment, Horsemouth 
+};
