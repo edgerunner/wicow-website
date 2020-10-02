17c813a272ff12b40f1c25c6abfc80c569d50f14
Mert Torun <mert@merttorun.com>
2020-10-02 15:05:00 +0300

    Implement a <ReactiveNumber> component
    Uses <Number> internally.
    `type`: the XState event type that this component listens for
    `field`: the key in the event object that the number is set to
    `fallback`: passed to <Number> directly

---
+++ components/ReactiveNumber.js
@@ -0,0 +1,27 @@
+import { useEventBus } from "../hooks";
+import { useMachine } from '@xstate/react';
+import { Machine, assign } from "xstate";
+import { Number } from "./Number";
+
+export default function ReactiveNumber({ type, field, fallback }) {
+    const bus = useEventBus();
+    const [state] = useMachine(Machine({
+        id: "ReactiveNumber",
+        initial: "number",
+        states: { number: {} },
+        context: { number: null },
+        on: {
+            [type]: {
+                actions: assign({
+                    number: (context, event) => event[field]
+                })
+            }
+        },
+        invoke: { id: "bus", src: "bus" }
+    }), {
+        devTools: true,
+        services: { bus }
+    });
+
+    return <Number value={state.context.number} fallback={fallback}/>
+}
