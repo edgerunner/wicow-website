5b09e0d1e553ebd53b42c3ea4d96eab49565231f
Mert Torun <mert@merttorun.com>
2020-10-02 16:43:11 +0300

    Implement localized <Currency> component to display yearly loss

--- components/ReactiveNumber.js
+++ components/ReactiveNumber.js
@@ -1,9 +1,15 @@
 import { useReactiveValue } from "../hooks";
 import { Number } from "./Number";
+import { Currency } from "./Currency";
 
 export default {
     CowCount() {
         const count = useReactiveValue("UPDATE_COW_COUNT", "count");
         return <Number value={count} fallback="100"/>;
+    },
+    YearlyLoss() {
+        const count = useReactiveValue("UPDATE_COW_COUNT", "count");
+        const loss = count * 80; // Euros
+        return <Currency value={loss} unit="EUR" fallback="€8.000"/>
     }
 }
