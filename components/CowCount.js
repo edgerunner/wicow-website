aa51377ba08bea3a8508893241ecc3d31bbe0934
Mert Torun <mert@merttorun.com>
2020-10-05 12:51:45 +0300

    Extract <CowCount> CSS to global

--- components/CowCount.js
+++ components/CowCount.js
@@ -39,7 +39,7 @@ export default function CowCount(props) {
         props.onChange?.(positionToValue(event.target.value))
     }, [position, props.onChange]);
 
-    return <div> 
+    return <div className="CowCount"> 
         <input type="range" 
             id={props.id}
             list="cow-count" 
@@ -63,79 +63,5 @@ export default function CowCount(props) {
             <option value={5}>500</option>
             <option value={6}>1000</option>
         </datalist>
-        <style jsx>{`
-            div {
-                position: relative;
-                z-index: 0;
-            }
-            input[type="range"] {
-                appearance: none;
-                background: transparent;
-                width: 100%;
-                height: 4rem;
-                border: solid .25rem var(--darkish);
-                border-radius: .5rem;
-                margin: 0;
-                padding: .25rem;
-            }
-            
-            input[type="range"]:focus {
-                outline: none;
-                box-shadow: 0 0 0 .03125rem var(--light)
-            }
-
-            input[type=range]::-webkit-slider-thumb {
-                appearance: none;
-                height: 3rem;
-                width: 14.28%;
-                border-radius: .25rem;
-                background: var(--wicow-blue);
-                border: none;
-                cursor: pointer;
-                margin: 0; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
-                border: 1px solid var(--dark);
-            }
-            
-            input[type=range]::-moz-range-thumb {
-                appearance: none;
-                height: 3rem;
-                width: 14.28%;
-                border-radius: .25rem;
-                background: var(--wicow-blue);
-                border: none;
-                cursor: pointer;
-                margin: 0;
-                border: 1px solid var(--dark);
-            }
-
-            datalist {
-                display: flex;
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                padding: 0 .5rem;
-                align-items: center;
-                justify-content: space-around;
-                z-index: -1;
-                background: var(--dark);
-            }
-            datalist > option {
-                display: block;
-                color: var(--darkish);
-                text-align: center;
-                width: 5ch;
-            }
-            output {
-                position: absolute;
-                width: 14.28%;
-                top: .5em;
-                bottom: 0;
-                text-align: center;
-                pointer-events: none;
-                color: var(--dark);
-            }
-        `}</style>
     </div>;
 }
