e6426851a514c612fbf4c0e114add05416baa646
Mert Torun <mert@merttorun.com>
2020-10-07 04:05:03 +0300

    Make a state machine for <AlertsSummary>

--- components/AlertsSummary.js
+++ components/AlertsSummary.js
@@ -1,10 +1,82 @@
 import { useCallback, useEffect, useReducer } from "react";
 import { useTranslation } from "../hooks";
+import { Machine, assign, actions } from "xstate";
 import translations from "./AlertsSummary.yaml";
 import { Number } from "./Number";
 
 const polling_timeout = process.env.NEXT_PUBLIC_FETCH_POLLING_INTERVAL;
 const retry_timeout = process.env.NEXT_PUBLIC_FETCH_RETRY_INTERVAL;
+
+const machine = Machine({
+    id: "AlertsSummary",
+    context: {},
+    type: "parallel",
+    states: {
+        data: {
+            initial: "none",
+            states: {
+                none: {},
+                some: {
+                    initial: "fresh",
+                    states: {
+                        fresh: {
+                            after: {
+                                polling_timeout: "stale"
+                            }
+                        },
+                        stale: {
+                            entry: actions.send("STALE")
+                        }
+                    }
+                }
+            },
+            on: {
+                SUCCESS: {
+                    target: "data.some.fresh",
+                    actions: assign((ctx,event) => event.data)
+                }
+            }
+        },
+        transfer: {
+            initial: "pending",
+            states: {
+                pending: {
+                    invoke: {
+                        src: "fetchAlerts",
+                        onDone: {
+                            target: "done",
+                            actions: actions.raise((ctx, event) => ({
+                                type: "SUCCESS",
+                                data: event.data
+                            }))
+                        },
+                        onError: {
+                            target: "failed",
+                            actions: actions.log((ctx, event) => event.data)
+                        }
+                    }
+                },
+                done: {},
+                failed: {
+                    after: {
+                        retry_timeout: "pending"
+                    }
+                }
+            },
+            on: {
+                STALE: ".pending"
+            }
+        }
+    }
+}, {
+    delays: { 
+        retry_timeout: parseInt(process.env.NEXT_PUBLIC_FETCH_RETRY_INTERVAL), 
+        polling_timeout: parseInt(process.env.NEXT_PUBLIC_FETCH_POLLING_INTERVAL) 
+    },
+    services: {
+        async fetchAlerts() { return (await fetch("/api/alerts")).json(); }
+    }
+});
 
 export default function AlertsSummary() {
     const keys = useTranslation(translations);
