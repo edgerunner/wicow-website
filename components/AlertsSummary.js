f3dfba7d70882c99c8d600e2a04db66b1a489965
Mert Torun <mert@merttorun.com>
2020-10-07 04:13:35 +0300

    Wire up <AlertsSummary> to its state machine
    Get rid of React useReducer and friends

--- components/AlertsSummary.js
+++ components/AlertsSummary.js
@@ -1,11 +1,8 @@
-import { useCallback, useEffect, useReducer } from "react";
 import { useTranslation } from "../hooks";
 import { Machine, assign, actions } from "xstate";
+import { useMachine } from "@xstate/react";
 import translations from "./AlertsSummary.yaml";
 import { Number } from "./Number";
-
-const polling_timeout = process.env.NEXT_PUBLIC_FETCH_POLLING_INTERVAL;
-const retry_timeout = process.env.NEXT_PUBLIC_FETCH_RETRY_INTERVAL;
 
 const machine = Machine({
     id: "AlertsSummary",
@@ -81,51 +78,22 @@ const machine = Machine({
 export default function AlertsSummary() {
     const keys = useTranslation(translations);
 
-    const [state, dispatch] = useReducer(reducer, { initial: true });
-
-    const getAlerts = useCallback(() => {
-        dispatch({type: "start"});
-        fetchAlerts()
-            .then(data => dispatch({type: "success", data}))
-            .catch(error => dispatch({type: "error", error}));
-    }, [dispatch]);
-
-    useEffect(() => {
-        if (state.initial) { getAlerts() }
-        if (state.error) { return timeout(getAlerts, retry_timeout); }
-        if (state.data) { return timeout(getAlerts, polling_timeout); }
-    }, [state]);
-
-
+    const [state] = useMachine(machine, { devTools: true })
 
     return <aside className="AlertsSummary">
         <dl>
             <dt>{keys.perfect}</dt>
-            <dd><em><Number value={state.data?.perfect} fallback="…"/></em></dd>
+            <dd><em><Number value={state.context?.perfect} fallback="…"/></em></dd>
 
             <dt>{keys.eventual}</dt>
-            <dd><Number value={state.data?.eventual} fallback="…"/></dd>
+            <dd><Number value={state.context?.eventual} fallback="…"/></dd>
 
             <dt>{keys.false_}</dt>
-            <dd><Number value={state.data?.false_} fallback="…"/></dd>
+            <dd><Number value={state.context?.false_} fallback="…"/></dd>
 
             <dt>{keys.missed}</dt>
-            <dd><Number value={state.data?.missed} fallback="…"/></dd>
+            <dd><Number value={state.context?.missed} fallback="…"/></dd>
         </dl>
     </aside>;
 }
 
-async function fetchAlerts() { return (await fetch("/api/alerts")).json(); }
-
-function timeout(callback, duration) {
-    const id = setTimeout(callback, duration);
-    return () => clearTimeout(id);
-}
-
-function reducer(state, action) {
-    switch (action.type) {
-        case "success": return { data: action.data };
-        case "error": return { error: action.error, data: state.data };
-        case "start": return { loading: true, error: state.error, data: state.data };
-    }
-}
